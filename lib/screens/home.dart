75cf683695c588d3ee3f415a5a7efc0bb20a7ffc
MasterKavi-24 <srikavipriyan3@gmail.com>
2023-03-30 21:18:12 +0530

    Other commits

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:gdsc_soln_23/screens/feed.dart';
 import 'package:gdsc_soln_23/screens/intro.dart';
 import 'package:gdsc_soln_23/widgets/feedCard.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
+
 
 class Home extends StatefulWidget {
 
@@ -29,11 +31,24 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<void> _handleRefresh() async {
+    // reloading take some time
+    return await Future.delayed(Duration(seconds: 2));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: _widgetOptions.elementAt(_selectedIndex),
+      body: LiquidPullToRefresh(
+        onRefresh: _handleRefresh,
+        color: Colors.grey,
+        height: 200,
+        backgroundColor: Colors.grey[200],
+        animSpeedFactor: 2, // faster
+        // showChildOpacityTransition: false, // shows the child
+        child: SafeArea(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
       ),
 
       bottomNavigationBar: BottomNavigationBar(
@@ -66,3 +81,67 @@ class _HomeState extends State<Home> {
     );
   }
 }
+
+
+/*
+// requires liquid_pull_to_refresh package
+import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
+
+class FancyRefresh extends StatelessWidget {
+  
+  Future<void> _handleRefresh() async {
+    // reloading take some time
+    return await Future.delayed(Duration(seconds: 2));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: LiquidPullToRefresh(
+        onRefresh: _handleRefresh,
+        color: Colors.grey,
+        height: 200,
+        backgroundColor: Colors.grey[200],
+        animSpeedFactor: 2, // faster
+        // showChildOpacityTransition: false, // shows the child
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Container(
+                height: 300,
+                color: Colors.grey,
+                )
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Container(
+                height: 300,
+                color: Colors.grey,
+                )
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Container(
+                height: 300,
+                color: Colors.grey,
+                )
+              ),
+            ),
+          ]
+        ),
+      )
+    );
+  }
+}
+*/
